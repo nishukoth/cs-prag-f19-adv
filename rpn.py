5aa52137884da44c7e7cc7cb100323100099cc5f
nishuk <nishuk@umich.edu>
2019-11-06 23:05:12 -0500

    Added colored text function for negative results
    Utilized the colorama module in order to add color negative results in red

--- rpn.py
+++ rpn.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import operator
-
+import colorama
+from colorama import Fore, Back, Style
+#from termcolor import colored
+colorama.init()
 
 #operators = {
     #'+': operator.add,
@@ -49,7 +52,7 @@ def calculate(myarg):
                 arg1 = stack.pop()
                 result = operator.mod(arg1, arg2)
                 stack.append(result)
-        print(stack)
+        #print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
@@ -57,7 +60,11 @@ def calculate(myarg):
 def main():
     while True:
         result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        if result < 0:
+            print("Result: " + Fore.RED + str(result))
+            print(Style.RESET_ALL)
+        else:
+            print("Result: ", result)
 
 if __name__ == '__main__':
     main()
