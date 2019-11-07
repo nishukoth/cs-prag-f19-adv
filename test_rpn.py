55902bf7c9494ee9935ea6a7c5bb06f19fa66433
nishuk <nishuk@umich.edu>
2019-11-06 22:07:56 -0500

    Commented out most of the tests in order to demonstrate that coverage is working

--- test_rpn.py
+++ test_rpn.py
@@ -2,22 +2,23 @@ import pytest
 
 import rpn
 
-
-def test_add():
-    result = rpn.calculate("1 1 +")
-    assert 2 == result
-def test_subtract():
-    result = rpn.calculate("5 3 -")
-    assert 2 == result
-def test_multiply():
-    result = rpn.calculate("5 3 *")
-    assert 15 == result
-def test_divide():
-    result = rpn.calculate("6 3 /")
-    assert 2 == result
-def test_exponent():
-    result = rpn.calculate("4 3 ^")
-    assert 64 == result
+def test_pass():
+    assert 2 == 2
+#def test_add():
+    #result = rpn.calculate("1 1 +")
+    #assert 2 == result
+#def test_subtract():
+    #result = rpn.calculate("5 3 -")
+    #assert 2 == result
+#def test_multiply():
+    #result = rpn.calculate("5 3 *")
+    #assert 15 == result
+#def test_divide():
+    #result = rpn.calculate("6 3 /")
+    #assert 2 == result
+#def test_exponent():
+    #result = rpn.calculate("4 3 ^")
+    #assert 64 == result
 #def test_modulus():
     #result = rpn.calculate("19 5 %")
     #assert 4 == result
